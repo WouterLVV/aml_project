76a7e5ef636a2792db14c0b8cfb36fff98fe9cdb
Wouter van Veelen <wouterlvv@gmail.com>
2018-12-06 19:49:12 +0100

    started agent class and made file for game class

---
+++ logic/agent.py
@@ -0,0 +1,14 @@
+class Agent:
+
+    def __init__(self, hand):
+        self.hand = hand
+        self.played = []
+
+    def make_move(self, table):
+        pass
+
+    def pick_card(self, table):
+        return 0
+
+    def check_valid(self, card, table):
+        pass
