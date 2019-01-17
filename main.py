df40aa9b50526447599dd332f649abd6f38318b6
TJHeeringa <heeringa@protonmail.ch>
2019-01-17 15:53:21 +0100

    Changed decay_rate

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ import tensorflow as tf
 start_time = time.time()
 with tf.Session() as tensorflow_session:
     neural_network = DQNetwork(161, 52, [200, 100, 100], ['sigmoid', 'elu', 'relu', 'sigmoid'], 0.3)
-    players = [ml_agent(neural_network=neural_network, tensorflow_session=tensorflow_session),
-               ml_agent(neural_network=neural_network, tensorflow_session=tensorflow_session),
-               ml_agent(neural_network=neural_network, tensorflow_session=tensorflow_session),
-               ml_agent(neural_network=neural_network, tensorflow_session=tensorflow_session)]
+    players = [ml_agent(neural_network=neural_network, tensorflow_session=tensorflow_session, decay_rate=0.001),
+               ml_agent(neural_network=neural_network, tensorflow_session=tensorflow_session, decay_rate=0.001),
+               ml_agent(neural_network=neural_network, tensorflow_session=tensorflow_session, decay_rate=0.001),
+               ml_agent(neural_network=neural_network, tensorflow_session=tensorflow_session, decay_rate=0.001)]
     simulator = Simulator(players=players,
                           number_of_games_per_cycle=50,
                           number_of_update_cycles=100,
