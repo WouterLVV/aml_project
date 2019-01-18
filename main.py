a89c2c12ebd25692bfc79cdf89f9ebe7f3b6791a
TJHeeringa <heeringa@protonmail.ch>
2019-01-18 10:46:30 +0100

    Big test

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ start_time = time.time()
 with tf.Session() as tensorflow_session:
     neural_network = DQNetwork(state_size=161,
                                action_size=52,
-                               hidden_sizes=[500, 250, 250, 250, 300],
+                               hidden_sizes=[250, 200, 200, 150, 100],
                                layer_activation_functions=['relu', 'relu', 'relu', 'relu', 'relu', 'softmax'],
                                learning_rate=0.3)
     saver = tf.train.Saver()
@@ -18,17 +18,11 @@ with tf.Session() as tensorflow_session:
                ml_agent(neural_network=neural_network, tensorflow_session=tensorflow_session, decay_rate=0.000),
                ml_agent(neural_network=neural_network, tensorflow_session=tensorflow_session, decay_rate=0.000)]
     simulator = Simulator(players=players,
-                          number_of_games_per_cycle=1000,
-                          number_of_update_cycles=20,
+                          number_of_games_per_cycle=100000,
+                          number_of_update_cycles=1,
                           neural_network=neural_network,
-                          update_rate=0.4,
+                          update_rate=0.9,
                           tensorflow_session=tensorflow_session)
-    simulator.run_cycles()
-    for player in players:
-        player.games_played = 0
-        player.decay_rate = 0.001
-    simulator.number_of_games = 100
-    simulator.number_of_update_cycles = 30
     simulator.run_cycles()
     saver.save(tensorflow_session, './saved_tensorflow_sessions/tensorflow_session')
 
